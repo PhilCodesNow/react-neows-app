e0673d608c9b5bb70e92f1086e3fae9de8589340
PhilCodesNow <philipmayojr@outlook.com>
2021-05-18 21:51:16 -0400

    got basic api call return

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
+import { useState, useEffect } from 'react';
+import Results from './Results';
 import './App.css';
 
+
+
 function App() {
+
+const [apiData, setApiData] = useState([])
+
+console.log('api data')
+console.log(apiData)
+
+
+
+useEffect(() =>{
+  // fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY`)
+  // .then(response => response.json())
+  // .then(data => setApiData(data))
+}, [])
+
+
+
+
+
+
+
   return (
     <div className="App">
       REACT
+      <Results
+      apiData={apiData}
+      />
     </div>
   );
 }
