72b05e51840e41c77ae99365735d9c38b05b7141
PhilCodesNow <philipmayojr@outlook.com>
2021-05-23 14:15:44 -0400

    got more view page styling complete

--- src/View.js
+++ src/View.js
@@ -16,19 +16,54 @@ function View(props) {
             hazardous = 'false'
         }
 
+
+        const ShortenNum = (number) =>{
+            let minDiameter = number
+            let minDiameterString = `${minDiameter}`
+            let minDiameterArray = [minDiameterString]
+            let minDiameterArraySplit = minDiameterArray[0].split('')
+            let newMinDiameterArray = [minDiameterArraySplit[0], minDiameterArraySplit[1], minDiameterArraySplit[2], minDiameterArraySplit[3]]
+            let returnedNum = newMinDiameterArray.join('')
+            return returnedNum
+        }
+
+        const shortenToDecimalPoint = (number) =>{
+            console.log(number)
+            let string = `${number}`
+            console.log(string)
+            let array = [string]
+            console.log(array)
+             let arraySplit = array[0].split('')
+            console.log(arraySplit)
+            let numOfIs = (arraySplit.indexOf('.') + 3)
+            console.log(numOfIs)
+            let newArray = arraySplit.slice(0 ,numOfIs)
+            console.log(newArray)
+            let returnAnswer = newArray.join('')
+            console.log(returnAnswer)
+            return returnAnswer
+        }
+        
+        shortenToDecimalPoint(selectedNeo.close_approach_data[0].miss_distance.miles)
+
         return (
             <div className="view">
 
-                <div className="view__header"><h2>{selectedNeo.name}</h2></div>
-                <div>Estimated Diameter: {selectedNeo.estimated_diameter.miles.estimated_diameter_min} miles - {selectedNeo.estimated_diameter.miles.estimated_diameter_max} miles</div>
-                <div>
-                    Distance NEO Will Miss Earth By: {selectedNeo.close_approach_data[0].miss_distance.kilometers}
+                <div className="view__header">
+                    <h2>{selectedNeo.name}</h2>
                 </div>
-                <div>
+                <div className="view__div">
+                    Estimated Object Diameter: {ShortenNum(selectedNeo.estimated_diameter.miles.estimated_diameter_min)} miles - {ShortenNum(selectedNeo.estimated_diameter.miles.estimated_diameter_max)} miles
+                </div>
+
+                <div className="view__div">
+                    Distance NEO Will Miss Earth By: {shortenToDecimalPoint(selectedNeo.close_approach_data[0].miss_distance.miles)} miles
+                </div>
+                <div className="view__div">
                     Potentially Hazardous: {hazardous}
                 </div>
-                <div>
-                    NEO Speed: {selectedNeo.close_approach_data[0].relative_velocity.miles_per_hour} mph
+                <div className="view__div">
+                    NEO Speed: {shortenToDecimalPoint(selectedNeo.close_approach_data[0].relative_velocity.miles_per_hour)} mph
                 </div>
 
                 <button onClick={handleUnselectNeo}>Close</button>
